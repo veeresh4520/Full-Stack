8ff4d0917966280464e342507537089610c7f1cd
Veeresh <veereshyadav74103@gmail.com>
2026-07-30 08:58:40 +0530

    Added index.html

---
+++ First.js
@@ -0,0 +1,6 @@
+veeresh =  {
+    name:"Veeeresh",
+    age:28,
+    developer:true
+}
+module.exports = veeresh
---
+++ index.js
@@ -0,0 +1,5 @@
+const athar = require("./First");
+console.log("Hellow Bhai",athar);
+const os = require("os")
+console.log(os.freemem())
+console.log(os.homedir())
